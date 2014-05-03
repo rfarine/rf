59530f63cc3dcbcd183c6b2ff5889c0c6ab74790
Rae <info@rae-farine.net>
2014-05-03 17:19:37 -0400

    Adding Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+	include PostsHelper
+
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.save
+
+		flash.notice = "Post '#{@post.title}' Created!"
+
+		redirect_to post_path(@post)
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		flash.notice = "Post '#{@post.title}' Deleted!"
+
+		redirect_to posts_path
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+
+		flash.notice = "Post '#{@post.title}' Updated!"
+
+		redirect_to post_path(@post)
+	end
+
+end
