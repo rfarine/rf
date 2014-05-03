cf98f4c8a49183fb6b78cead0bb1bbc0d0347269
Rae <info@rae-farine.net>
2014-05-03 18:41:16 -0400

    Adding Paperclip.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140503220850) do
+ActiveRecord::Schema.define(version: 20140503223325) do
+
+  create_table "attachments", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.string   "author_name"
